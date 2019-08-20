64d1ec49e65374a0f0ad512de53f98ed11b1b8bf
Michael Fink <mgfink@yahoo.com>
2019-08-20 11:40:23 -0500

    Updated using statement for proper namespace

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using System.Diagnostics.Trace;
+using System.Diagnostics;
 
 namespace aspnetcoreapp
 {
